2d8701686eeee6478f4f0089b0e65f7d93c83924
Brian Deale <bhdeale@gmail.com>
2020-03-29 11:16:38 -0400

    fixing typos and syntax issues fixed start location for red legion fixed some descriptions added blueprints nexerelin corvus start is working custom start fails cleanly

--- data/scripts/campaign/customstart/CrimsonGuardStart.java
+++ data/scripts/campaign/customstart/CrimsonGuardStart.java
@@ -6,11 +6,15 @@ import com.fs.starfarer.api.campaign.SpecialItemData;
 import com.fs.starfarer.api.campaign.rules.MemKeys;
 import com.fs.starfarer.api.campaign.rules.MemoryAPI;
 import com.fs.starfarer.api.characters.CharacterCreationData;
+import com.fs.starfarer.api.characters.MutableCharacterStatsAPI;
 import com.fs.starfarer.api.impl.campaign.ids.Commodities;
 import com.fs.starfarer.api.impl.campaign.ids.Factions;
+import com.fs.starfarer.api.impl.campaign.rulecmd.AddRemoveCommodity;
 import com.fs.starfarer.api.impl.campaign.rulecmd.FireBest;
 import com.fs.starfarer.api.impl.campaign.rulecmd.newgame.NGCAddStartingShipsByFleetType;
 import static com.fs.starfarer.api.impl.campaign.rulecmd.newgame.NGCAddStartingShipsByFleetType.addStartingDModScript;
+
+import exerelin.campaign.ExerelinSetupData;
 import exerelin.campaign.PlayerFactionStore;
 import exerelin.campaign.customstart.CustomStart;
 
@@ -32,20 +36,34 @@ public class CrimsonGuardStart extends CustomStart {
 
     @Override
     public void execute(InteractionDialogAPI dialog, Map<String, MemoryAPI> memoryMap) {
-        PlayerFactionStore.setPlayerFactionIdNGC("redlegion");
-
         CharacterCreationData data = (CharacterCreationData) memoryMap.get(MemKeys.LOCAL).get("$characterData");
+
+        PlayerFactionStore.setPlayerFactionIdNGC("redlegion");
+        data.setDifficulty("normal");
+        ExerelinSetupData.getInstance().easyMode = false;
 
         NGCAddStartingShipsByFleetType.generateFleetFromVariantIds(dialog, data, null, ships);
 
         addStartingDModScript(memoryMap.get(MemKeys.LOCAL));
+        data.getStartingCargo().getCredits().add(250000);
+        AddRemoveCommodity.addCreditsGainText(250000, dialog.getTextPanel());
         data.getStartingCargo().addItems(CargoAPI.CargoItemType.RESOURCES, Commodities.CREW, 600);
+        AddRemoveCommodity.addCommodityGainText(Commodities.CREW, 600, dialog.getTextPanel());
         data.getStartingCargo().addItems(CargoAPI.CargoItemType.RESOURCES, Commodities.SUPPLIES, 350);
+        AddRemoveCommodity.addCommodityGainText(Commodities.SUPPLIES, 350, dialog.getTextPanel());
         data.getStartingCargo().addItems(CargoAPI.CargoItemType.RESOURCES, Commodities.FUEL, 500);
+        AddRemoveCommodity.addCommodityGainText(Commodities.FUEL, 500, dialog.getTextPanel());
         data.getStartingCargo().addItems(CargoAPI.CargoItemType.RESOURCES, Commodities.ALPHA_CORE, 1);
+        AddRemoveCommodity.addCommodityGainText(Commodities.ALPHA_CORE, 1, dialog.getTextPanel());
         data.getStartingCargo().addItems(CargoAPI.CargoItemType.RESOURCES, Commodities.MARINES, 150);
+        AddRemoveCommodity.addCommodityGainText(Commodities.MARINES, 150, dialog.getTextPanel());
         data.getStartingCargo().addItems(CargoAPI.CargoItemType.RESOURCES, Commodities.HEAVY_MACHINERY, 50);
+        AddRemoveCommodity.addCommodityGainText(Commodities.HEAVY_MACHINERY, 50, dialog.getTextPanel());
         data.getStartingCargo().addItems(CargoAPI.CargoItemType.RESOURCES, Commodities.HAND_WEAPONS, 25);
+        AddRemoveCommodity.addCommodityGainText(Commodities.HAND_WEAPONS, 25, dialog.getTextPanel());
+
+        MutableCharacterStatsAPI stats = data.getPerson().getStats();
+        stats.addPoints(3);
 
         FireBest.fire(null, dialog, memoryMap, "ExerelinNGCStep4");
     }
