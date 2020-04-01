7f8c5360fd87cc12031f398c876e311c12dccd21
Brian Deale <bhdeale@gmail.com>
2020-04-01 11:41:23 -0400

    Adding tradewind to generation (corvus and vanilla)

--- data/scripts/world/RedLegionGen.java
+++ data/scripts/world/RedLegionGen.java
@@ -6,7 +6,7 @@ import com.fs.starfarer.api.campaign.SectorAPI;
 import data.scripts.world.systems.Argon;
 //import data.scripts.world.systems.Dawn;
 //import data.scripts.world.systems.Gale;
-//import data.scripts.world.systems.Tradewind;
+import data.scripts.world.systems.Tradewind;
 
 public class RedLegionGen {
 
@@ -14,7 +14,7 @@ public class RedLegionGen {
             (new Argon()).generate(sector);
 //            (new Dawn()).generate(sector);
 //            (new Gale()).generate(sector);
-//            (new Tradewind()).generate(sector);
+            (new Tradewind()).generate(sector);
                                                
         }
 
